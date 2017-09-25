3419b48b7b73171c40317574f877b479f7d4e3b5
FTCRobocat-BTeam-Mr.G <31868494+FTCRobocat@users.noreply.github.com>
2017-09-25 17:43:01 -0500

    TeamBTeleOp

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TeamBTeleOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TeamBTeleOp.java
@@ -1,21 +1,23 @@
-
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
 @Disabled
-public class BasicOpMode_Linear extends LinearOpMode {
+public class TeamBTeleOp extends LinearOpMode {
 
     // Declare OpMode members.
     public ElapsedTime runtime = new ElapsedTime();
-    public DcMotor leftDrive = null;
-    public DcMotor rightDrive = null;
+    public DcMotor FleftDrive = null;
+    public DcMotor FrightDrive = null;
+    public DcMotor BleftDrive = null;
+    public DcMotor BrightDrive = null;
+    
 
     @Override
     public void runOpMode() {
@@ -25,13 +27,18 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        FleftDrive = hardwareMap.get(DcMotor.class, "FleftDrive");
+        FrightDrive = hardwareMap.get(DcMotor.class, "FrightDrive");
+        BleftDrive = hardwareMap.get(DcMotor.class, "BleftDrive");
+        BrightDrive = hardwareMap.get(DcMotor.class, "BrightDrive");
+
 
         // Most robots need the motor on one side to be reversed to drive forward
         // Reverse the motor that runs backwards when connected directly to the battery
-        leftDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        FleftDrive.setDirection(DcMotor.Direction.FORWARD);
+        BleftDrive.setDirection(DcMotor.Direction.FORWARD);
+        FrightDrive.setDirection(DcMotor.Direction.REVERSE);
+        BrightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -43,7 +50,7 @@ public class BasicOpMode_Linear extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
-
+            /*
             // Choose to drive using either Tank Mode, or POV Mode
             // Comment out the method that's not used.  The default below is POV.
 
@@ -53,15 +60,27 @@ public class BasicOpMode_Linear extends LinearOpMode {
             double turn  =  gamepad1.right_stick_x;
             leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
             rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
+            */
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+            leftPower  = -gamepad1.left_stick_y;
+            rightPower = -gamepad1.right_stick_y;
 
-            // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+
+            if (leftPower < 0 && rightPower < 0) { //Sets the front motors to 0 because of the omni wheels
+                FleftDrive.setPower(0);
+                FrightDrive.setPower(0);
+                BleftDrive.setPower(leftPower);
+                BrightDrive.setPower(rightPower);
+            }
+            else {
+                // Send calculated power to wheels
+                FleftDrive.setPower(leftPower);
+                FrightDrive.setPower(rightPower);
+                BleftDrive.setPower(leftPower);
+                BrightDrive.setPower(rightPower);
+            }
+
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
